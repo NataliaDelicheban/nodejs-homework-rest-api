65cf047f83778ad60f17480da434e8b6fe6d1515
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-11-06 17:37:16 +0100

    create hw 5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const { ctrlWrapper } = require("../../helpers");
 
 const ctrl = require("../../controllers/auth")
 
-const { validateBody, authenticate } = require("../../middlewares")
+const { validateBody, authenticate, upload } = require("../../middlewares")
 
 const { schemas } = require("../../models/user")
 
@@ -17,5 +17,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login)
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent))
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout))
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar))
 
 module.exports = router;
